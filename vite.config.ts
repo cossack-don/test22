0835ccf51a872e9ee6d6da510e8f26c6754e8821
sergey_zhuravlev <ser90593696.zhuravlev@yandex.ru>
2023-07-23 20:56:34 +0300

    ip

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import checker from 'vite-plugin-checker';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(),    checker({
+      typescript: true,
+    }),
   ],
   resolve: {
     alias: {
